398d6882b0e2974e7a9f9ff323eda7d05af1f0d8
Anil Vidya Sagar Saila <anils@ispace.com>
2020-01-25 16:04:24 +0530

    deployement-prod-ai

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 //app insight
 app.use('/problem', function() {
