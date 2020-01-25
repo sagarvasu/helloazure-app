3bea12c841b9ec5fc7fab4d289928ac4ac879e3d
Anil Vidya Sagar Saila <anils@ispace.com>
2020-01-25 12:22:42 +0530

    slot-deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure' });
+  //res.render('index', { title: 'Azure' });
+
+  var model = {
+    title: 'Azure',
+    message: process.env.MESSAGE || "This is development"
+  };
+  res.render('index', model);
 });
 
 module.exports = router;
